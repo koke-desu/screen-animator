fbb088ff4165269ea493c356807d3654dc73b42b
koke-desu <gbpvp25645@yahoo.co.jp>
2024-04-01 10:31:38 +0900

    feat: 選択中のNodeをatomで管理

--- src/globalState/nodes.ts
+++ src/globalState/nodes.ts
@@ -12,11 +12,23 @@ export type NodeType = {
 };
 
 export const nodesAtom = atom<NodeType[]>([]);
+const selectedNodeIdAtom = atom<string | null>(null);
 
 export const addNodeAtom = atom(
   null,
   (get, set, node: Omit<NodeType, "id">) => {
     const id = crypto.randomUUID();
     set(nodesAtom, (prev) => [...prev, { ...node, id }]);
+  },
+);
+
+export const selectedNodeAtom = atom(
+  (get) => {
+    const nodes = get(nodesAtom);
+    const selectedNodeId = get(selectedNodeIdAtom);
+    return nodes.find((node) => node.id === selectedNodeId) || null;
+  },
+  (get, set, id: string) => {
+    set(selectedNodeIdAtom, id);
   },
 );
