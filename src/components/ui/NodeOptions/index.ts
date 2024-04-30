b8ebc09fc3b0dd09f9cd1fbb56f015b032d35a3e
koke-desu <gbpvp25645@yahoo.co.jp>
2024-04-30 19:45:18 +0900

    feat: Option周りの型を改修

--- src/components/ui/NodeOptions/index.ts
+++ src/components/ui/NodeOptions/index.ts
@@ -2,6 +2,7 @@ import NumberInput from "./NumberInput";
 import TextInput from "./TextInput";
 
 export type NodeOptionBaseType<T> = {
+  name: string;
   value: T;
   setValue: (value: T) => void;
 };
@@ -15,7 +16,18 @@ export type OptionComponentName = keyof typeof OptionComponents;
 export type OptionComponentProps<T extends OptionComponentName> =
   React.ComponentProps<(typeof OptionComponents)[T]>;
 export type OptionType<T extends OptionComponentName> = {
-  name: T;
+  type: T;
 } & OptionComponentProps<T>;
+export type OptionsType = OptionType<OptionComponentName>[];
+
+// Option定義時に型補助を効かせるためのヘルパー関数
+export const createOption = <T extends OptionComponentName>(
+  option: Omit<
+    OptionType<T>,
+    "value" | "setValue" // value, setValueはNodeのインスタンスから注入するため、Optionの定義にはいらない
+  >,
+) => {
+  return option as OptionType<T>; // うーん
+};
 
 export default OptionComponents;
